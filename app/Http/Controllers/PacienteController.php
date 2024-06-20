ef10945fe0b39dc92bf7994475cf06038f8e3260
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2024-06-20 20:02:53 -0300

    api paciente 1

---
+++ app/Http/Controllers/PacienteController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paciente;
+use Illuminate\Http\Request;
+
+class PacienteController extends Controller
+{
+    public function index()
+    {
+        return Paciente::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|string|max:255',
+            'idade' => 'required|integer',
+            'sexo' => 'required|string|max:1',
+            'sintomas' => 'required|string',
+        ]);
+
+        $paciente = Paciente::create($request->all());
+        return response()->json($paciente, 201);
+    }
+
+    public function show(Paciente $paciente)
+    {
+        return $paciente;
+    }
+
+    public function update(Request $request, Paciente $paciente)
+    {
+        $request->validate([
+            'nome' => 'sometimes|required|string|max:255',
+            'idade' => 'sometimes|required|integer',
+            'sexo' => 'sometimes|required|string|max:1',
+            'sintomas' => 'sometimes|required|string',
+        ]);
+
+        $paciente->update($request->all());
+        return response()->json($paciente, 200);
+    }
+
+    public function destroy(Paciente $paciente)
+    {
+        $paciente->delete();
+        return response()->json(null, 204);
+    }
+}
